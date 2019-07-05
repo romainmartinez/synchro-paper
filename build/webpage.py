5bc2a774d99e14c61beb9781d2c54d352e74eeb4
Romain Martinez <martinez.staps@gmail.com>
2019-07-05 13:44:39 -0400

    pycharm ignore and black python file

--- build/webpage.py
+++ build/webpage.py
@@ -11,28 +11,28 @@ def parse_arguments():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--checkout',
-        nargs='?', const='gh-pages', default=None,
-        help='branch to checkout /v directory contents from. For example, --checkout=upstream/gh-pages. --checkout is equivalent to --checkout=gh-pages. If --checkout is ommitted, no checkout is performed.',
+        "--checkout",
+        nargs="?",
+        const="gh-pages",
+        default=None,
+        help="branch to checkout /v directory contents from. For example, --checkout=upstream/gh-pages. --checkout is equivalent to --checkout=gh-pages. If --checkout is ommitted, no checkout is performed.",
     )
     parser.add_argument(
-        '--version',
-        default=os.environ.get('TRAVIS_COMMIT', 'local'),
+        "--version",
+        default=os.environ.get("TRAVIS_COMMIT", "local"),
         help="Used to create webpage/v/{version} directory. "
-             "Generally a commit hash, tag, or 'local'. "
-             "(default: '%(default)s')"
+        "Generally a commit hash, tag, or 'local'. "
+        "(default: '%(default)s')",
     )
     cache_group = parser.add_mutually_exclusive_group()
     cache_group.add_argument(
-        '--no-ots-cache',
-        action='store_true',
-        help="disable the timestamp cache."
+        "--no-ots-cache", action="store_true", help="disable the timestamp cache."
     )
     cache_group.add_argument(
-        '--ots-cache',
-        default=pathlib.Path('ci/cache/ots'),
+        "--ots-cache",
+        default=pathlib.Path("ci/cache/ots"),
         type=pathlib.Path,
-        help="location for the timestamp cache (default: %(default)s)."
+        help="location for the timestamp cache (default: %(default)s).",
     )
     args = parser.parse_args()
     return args
@@ -46,13 +46,13 @@ def configure_directories(args):
     args_dict = vars(args)
 
     # Directory where Manubot outputs reside
-    args_dict['output_directory'] = pathlib.Path('output')
+    args_dict["output_directory"] = pathlib.Path("output")
 
     # Set webpage directory
-    args_dict['webpage_directory'] = pathlib.Path('webpage')
+    args_dict["webpage_directory"] = pathlib.Path("webpage")
 
     # Create webpage/v directory (if it doesn't already exist)
-    args_dict['versions_directory'] = args.webpage_directory.joinpath('v')
+    args_dict["versions_directory"] = args.webpage_directory.joinpath("v")
     args.versions_directory.mkdir(exist_ok=True)
 
     # Checkout existing version directories
@@ -61,24 +61,24 @@ def configure_directories(args):
     # Create empty webpage/v/version directory
     version_directory = args.versions_directory.joinpath(args.version)
     if version_directory.is_dir():
-        print(f'{version_directory} exists: replacing it with an empty directory')
+        print(f"{version_directory} exists: replacing it with an empty directory")
         shutil.rmtree(version_directory)
     version_directory.mkdir()
-    args_dict['version_directory'] = version_directory
+    args_dict["version_directory"] = version_directory
 
     # Symlink webpage/v/latest to point to webpage/v/commit
-    latest_directory = args.versions_directory.joinpath('latest')
+    latest_directory = args.versions_directory.joinpath("latest")
     if latest_directory.is_symlink() or latest_directory.is_file():
         latest_directory.unlink()
     elif latest_directory.is_dir():
         shutil.rmtree(latest_directory)
     latest_directory.symlink_to(args.version, target_is_directory=True)
-    args_dict['latest_directory'] = latest_directory
+    args_dict["latest_directory"] = latest_directory
 
     # Create freeze directory
-    freeze_directory = args.versions_directory.joinpath('freeze')
+    freeze_directory = args.versions_directory.joinpath("freeze")
     freeze_directory.mkdir(exist_ok=True)
-    args_dict['freeze_directory'] = freeze_directory
+    args_dict["freeze_directory"] = freeze_directory
 
     return args
 
@@ -98,25 +98,29 @@ def checkout_existing_versions(args):
     if not args.checkout:
         return
     command = [
-        'git',
-        f'--work-tree={args.webpage_directory}',
-        'checkout',
+        "git",
+        f"--work-tree={args.webpage_directory}",
+        "checkout",
         args.checkout,
-        '--',
-        'v',
+        "--",
+        "v",
     ]
-    print('Attempting checkout with the following command:', ' '.join(command), sep='\n')
+    print(
+        "Attempting checkout with the following command:", " ".join(command), sep="\n"
+    )
     process = subprocess.run(command, stderr=subprocess.PIPE)
     if process.returncode == 0:
         # Addresses an odd behavior where git checkout stages v/* files that don't actually exist
-        subprocess.run(['git', 'add', 'v'])
+        subprocess.run(["git", "add", "v"])
     else:
         stderr = process.stderr.decode()
-        print(f'Checkout returned a nonzero exit status. See stderr:\n{stderr.rstrip()}')
-        if 'pathspec' in stderr:
+        print(
+            f"Checkout returned a nonzero exit status. See stderr:\n{stderr.rstrip()}"
+        )
+        if "pathspec" in stderr:
             print(
-                'Manubot note: if there are no preexisting webpage versions (like for a newly created manuscript), '
-                'the pathspec error above is expected and can be safely ignored.'
+                "Manubot note: if there are no preexisting webpage versions (like for a newly created manuscript), "
+                "the pathspec error above is expected and can be safely ignored."
             )  # see https://github.com/manubot/rootstock/issues/183
 
 
@@ -127,29 +131,23 @@ def create_version(args):
 
     # Copy content/images to webpage/v/commit/images
     shutil.copytree(
-        src=pathlib.Path('content/images'),
-        dst=args.version_directory.joinpath('images'),
+        src=pathlib.Path("content/images"),
+        dst=args.version_directory.joinpath("images"),
     )
 
     # Copy output files to to webpage/v/version/
-    renamer = {
-        'manuscript.html': 'index.html',
-        'manuscript.pdf': 'manuscript.pdf',
-    }
+    renamer = {"manuscript.html": "index.html", "manuscript.pdf": "manuscript.pdf"}
     for src, dst in renamer.items():
         src_path = args.output_directory.joinpath(src)
         if not src_path.exists():
             continue
-        shutil.copy2(
-            src=src_path,
-            dst=args.version_directory.joinpath(dst),
-        )
+        shutil.copy2(src=src_path, dst=args.version_directory.joinpath(dst))
 
     # Create v/freeze to redirect to v/commit
-    path = pathlib.Path('build/assets/redirect-template.html')
+    path = pathlib.Path("build/assets/redirect-template.html")
     redirect_html = path.read_text()
-    redirect_html = redirect_html.format(url=f'../{args.version}/')
-    args.freeze_directory.joinpath('index.html').write_text(redirect_html)
+    redirect_html = redirect_html.format(url=f"../{args.version}/")
+    args.freeze_directory.joinpath("index.html").write_text(redirect_html)
 
 
 def get_versions(args):
@@ -158,7 +156,7 @@ def get_versions(args):
     a manuscript.
     """
     versions = {x.name for x in args.versions_directory.iterdir() if x.is_dir()}
-    versions -= {'freeze', 'latest'}
+    versions -= {"freeze", "latest"}
     versions = sorted(versions)
     return versions
 
@@ -172,31 +170,25 @@ def ots_upgrade(args):
     """
     ots_paths = list()
     for version in get_versions(args):
-        ots_paths.extend(args.versions_directory.joinpath(version).glob('**/*.ots'))
+        ots_paths.extend(args.versions_directory.joinpath(version).glob("**/*.ots"))
     ots_paths.sort()
     for ots_path in ots_paths:
-        process_args = ['ots']
+        process_args = ["ots"]
         if args.no_ots_cache:
-            process_args.append('--no-cache')
+            process_args.append("--no-cache")
         else:
-            process_args.extend(['--cache', str(args.ots_cache)])
-        process_args.extend([
-            'upgrade',
-            str(ots_path),
-        ])
+            process_args.extend(["--cache", str(args.ots_cache)])
+        process_args.extend(["upgrade", str(ots_path)])
         process = subprocess.run(
-            process_args,
-            stderr=subprocess.PIPE,
-            universal_newlines=True,
+            process_args, stderr=subprocess.PIPE, universal_newlines=True
         )
         if process.returncode != 0:
-            print(f"OpenTimestamp upgrade command returned nonzero code ({process.returncode}).")
-        if not process.stderr.strip() == 'Success! Timestamp complete':
             print(
-                f">>> {' '.join(map(str, process.args))}\n"
-                f"{process.stderr}"
+                f"OpenTimestamp upgrade command returned nonzero code ({process.returncode})."
             )
-        backup_path = ots_path.with_suffix('.ots.bak')
+        if not process.stderr.strip() == "Success! Timestamp complete":
+            print(f">>> {' '.join(map(str, process.args))}\n" f"{process.stderr}")
+        backup_path = ots_path.with_suffix(".ots.bak")
         if backup_path.exists():
             if process.returncode == 0:
                 backup_path.unlink()
@@ -205,7 +197,7 @@ def ots_upgrade(args):
                 backup_path.rename(ots_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_arguments()
     configure_directories(args)
     print(args)
