147f1f484ec0a9937493bfb99d217acba8a022de
Romain Martinez <martinez.staps@gmail.com>
2019-07-05 11:02:06 -0400

    Brand repo to romainmartinez/synchro-paper

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -23,8 +23,8 @@ git remote set-url origin "git@github.com:$TRAVIS_REPO_SLUG.git"
 
 # Decrypt and add SSH key
 openssl aes-256-cbc \
-  -K $encrypted_9befd6eddffe_key \
-  -iv $encrypted_9befd6eddffe_iv \
+  -K $encrypted_a17ebe60ce8f_key \
+  -iv $encrypted_a17ebe60ce8f_iv \
   -in ci/deploy.key.enc \
   -out ci/deploy.key -d
 eval "$(ssh-agent -s)"
